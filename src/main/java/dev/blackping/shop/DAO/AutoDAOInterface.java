69e35594cfaab7c3721318d75960cfcbdfa6ebbb
BlackPing <47684708+BlackPing@users.noreply.github.com>
2019-09-22 23:41:47 +0900

    test

--- src/main/java/dev/blackping/shop/DAO/AutoDAOInterface.java
+++
@@ -1,9 +0,0 @@
-package dev.blackping.shop.DAO;
-
-import java.util.HashMap;
-
-import org.springframework.dao.DataAccessException;
-
-public interface AutoDAOInterface {
-	public HashMap<String, Object> getData(String type, String namespace, String id, Object bean) throws DataAccessException;
-}
