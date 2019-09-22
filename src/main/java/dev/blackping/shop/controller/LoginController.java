34c6487668e42688aca82d7b096963c2f0f0aa0b
BlackPing <47684708+BlackPing@users.noreply.github.com>
2019-09-22 23:45:41 +0900

    test

--- src/main/java/dev/blackping/shop/controller/LoginController.java
+++ src/main/java/dev/blackping/shop/controller/LoginController.java
@@ -1,17 +1,29 @@
 package dev.blackping.shop.controller;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginController {
 	@PostMapping(value="/login")
 	public HashMap<String, Object> Login(HttpServletResponse res) {
 		
 		return null;
+	}
+	
+	@GetMapping(value="/")
+	public void test(HttpServletResponse res) {
+		try {
+			res.getWriter().write("test");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
