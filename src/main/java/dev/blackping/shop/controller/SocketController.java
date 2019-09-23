293126f19c642bdcf21be3b405ecbb1c7e143395
BlackPing <47684708+BlackPing@users.noreply.github.com>
2019-09-23 21:26:31 +0900

    HandShake Interceptor

--- src/main/java/dev/blackping/shop/controller/SocketController.java
+++ src/main/java/dev/blackping/shop/controller/SocketController.java
@@ -1,6 +1,7 @@
 package dev.blackping.shop.controller;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SocketController {
 	@GetMapping(value="/1")
-	public String test(HttpServletResponse res) {
+	public String test(HttpServletResponse res, HttpSession session) {
+		session.setAttribute("Name", "test");
+		System.out.println(session.getAttribute("Name"));
 		return "echo1";
 	}
 }
