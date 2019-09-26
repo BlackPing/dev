c1ba4e5a9f0adf6b26b8e17dee72cc37180b3133
BlackPing <bug_ping@naver.com>
2019-09-26 09:45:36 +0900

    spring version change

--- src/main/java/dev/blackping/shop/controller/ViewController.java
+++ src/main/java/dev/blackping/shop/controller/ViewController.java
@@ -1,7 +1,12 @@
 package dev.blackping.shop.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
@@ -9,5 +14,15 @@ public class ViewController {
 	@GetMapping(value="/")
 	public String home() {
 		return "home";
+	}
+	
+	@RequestMapping(value="/tests")
+	public void aaaa(HttpServletResponse res) {
+		try {
+			res.getWriter().write("testest");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
