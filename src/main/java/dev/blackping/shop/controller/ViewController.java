d66ee10e5c63e62030fe646dd19a5f84972dc8a9
BlackPing <bug_ping@naver.com>
2019-09-26 10:30:58 +0900

    service test

--- src/main/java/dev/blackping/shop/controller/ViewController.java
+++ src/main/java/dev/blackping/shop/controller/ViewController.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import dev.blackping.shop.dao.AutoDAOInterface;
 
 @Controller
 public class ViewController {
@@ -16,10 +19,13 @@ public class ViewController {
 		return "home";
 	}
 	
+	@Autowired
+	AutoDAOInterface adi;
+	
 	@RequestMapping(value="/tests")
 	public void aaaa(HttpServletResponse res) {
 		try {
-			res.getWriter().write("testest");
+			res.getWriter().write(adi.sql("SO", "login", "test", null).toString());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
